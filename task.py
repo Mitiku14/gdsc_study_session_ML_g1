c4c3a0ffef0e55c5ea8806b2aaa33c07385e15e5
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2025-11-29 21:47:59 +0300

    Added NumPy array task script

---
+++ task.py
@@ -0,0 +1,11 @@
+import numpy as np 
+numbers = [10, 20, 30, 40, 50]
+arr = np.array(numbers)
+mean_value = np.mean(arr)
+max_value = np.max(arr)
+sum_value = np.sum(arr)
+print("Original List:", numbers)
+print("Numpy Array:", arr)
+print("Mean Value:", mean_value)
+print("Max Value:", max_value)
+print("Sum Value:", sum_value)
